89ba55b496a5014bcadfd6642ef416ade1b7fdf5
Wojciech Batorski <wojbat4@gmail.com>
2022-04-11 23:43:18 +0200

    adding new column to Item entity

--- src/main/java/com/spring/shoppbackend/model/Item.java
+++ src/main/java/com/spring/shoppbackend/model/Item.java
@@ -1,6 +1,8 @@
 package com.spring.shoppbackend.model;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Entity
 @Table(name = "items")
@@ -10,7 +12,7 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "itemName")
+    @Column(name = "item_name")
     private String itemName;
     @Column(name = "description")
     private String description;
@@ -18,16 +20,19 @@ public class Item {
     private double price;
     @Column(name = "quantity")
     private int quantity;
+    @Column(name = "item_added_date")
+    private Date itemAddedDate;
 
     public Item(){
 
     }
 
-    public Item(String itemName, String description, double price, int quantity){
+    public Item(String itemName, String description, double price, int quantity, Date itemAddedDate){
         this.itemName = itemName;
         this.description = description;
         this.price = price;
         this.quantity = quantity;
+        this.itemAddedDate = itemAddedDate;
     }
 
     public String getItemName() {
@@ -68,5 +73,13 @@ public class Item {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public Date getItemAddedDate() {
+        return itemAddedDate;
+    }
+
+    public void setItemAddedDate(Date itemAddedDate) {
+        this.itemAddedDate = itemAddedDate;
     }
 }
