5df9934b04f2bac09ff8c6534332fe61d33543cc
Durill <wojbat4@gmail.com>
2022-04-30 03:19:10 +0200

    add: custom query to fetch 3 newest items from DB

--- src/main/java/com/spring/shoppbackend/repository/ItemRepository.java
+++ src/main/java/com/spring/shoppbackend/repository/ItemRepository.java
@@ -2,8 +2,16 @@ package com.spring.shoppbackend.repository;
 
 import com.spring.shoppbackend.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query(value = "select * from items order by item_added_date desc limit ?;",
+            nativeQuery = true)
+    public List<Item> getNewestItems(Integer numberOfItems);
 }
